5e3b579818dfec4453e6fa23238bbbd6c2a87bc2
Erick Queiroz <erick.queiroz_@hotmail.com>
2015-05-10 17:50:30 -0300

    Score screen ended

--- score.lua
+++ score.lua
@@ -54,7 +54,7 @@ function scene:createScene(event)
   local sheet2 = graphics.newImageSheet( "images/deadbird2.png", { width=125, height=85, numFrames=4 } )
 
   --Cria o sprite do pássaro azul
-  deadbird2 = display.newSprite( sheet2, { name="bird", start=1, count=4, time=500 } )
+  deadbird2 = display.newSprite( sheet2, { name="bird2", start=1, count=4, time=500 } )
     deadbird2.x = _W/2 + 160
     deadbird2.y = _H/2 - 180
     deadbird2.xScale = 1.3
@@ -67,26 +67,79 @@ function scene:createScene(event)
   local sheet3 = graphics.newImageSheet( "images/finalbird.png", { width=125, height=90, numFrames=4 } )
 
   --Cria o sprite do pássaro cinza
-  finalbird = display.newSprite( sheet3, { name="bird", start=1, count=4, time=500 } )
-    finalbird.x = _W/2 + 160
+  finalbird = display.newSprite( sheet3, { name="bird3", start=1, count=4, time=500 } )
+    finalbird.x = -100
     finalbird.y = _H - 180
     finalbird.xScale = 1.3
     finalbird.yScale = 1.3
     finalbird:play()
     group:insert(finalbird)  
 
+  --Adiciona o pássaro cinza
+  local sheet4 = graphics.newImageSheet( "images/finalbird2.png", { width=125, height=90, numFrames=4 } )
+
+  --Cria o sprite do pássaro cinza
+  finalbird2 = display.newSprite( sheet4, { name="bird4", start=1, count=4, time=500 } )
+    finalbird2.x = _W + 100
+    finalbird2.y = _H - 180
+    finalbird2.xScale = 1.3
+    finalbird2.yScale = 1.3
+    finalbird2:play()
+    group:insert(finalbird2)    
+
   --Adiciona a bola de basquete
-  ball = display.newImage("images/ball.png")
-    ball.x = _W/2   
+  ball = display.newImage("images/ball.png")  
+    ball.x = -300      
     ball.y = _H-180 
     ball.xScale = 0.7
     ball.yScale = 0.7 
     group:insert(ball)  
-    
+
+  ball2 = display.newImage("images/ball2.png")
+    ball2.x = _W+300       
+    ball2.y = _H-180 
+    ball2.xScale = 0.7
+    ball2.yScale = 0.7 
+    group:insert(ball2)    
+   
+  --Cria animação para o pássaro e a bola  
   local function animate( event )
     transition.to( ball, { rotation = ball.rotation +360, time=600, onComplete=animate } )
   end
   animate()
+
+  local function animate2( event )
+    transition.to( ball2, { rotation = ball2.rotation -360, time=600, onComplete=animate2 } )
+  end
+  animate2()
+
+  local function ball_position()
+    ball.x = -300
+    ball2.x = _W + 300
+    move_ball()
+  end
+
+  function move_ball()
+    local move_ballback = function()
+      transition.to( ball2, {x=-100, time=3000, onComplete=ball_position} )
+    end
+      transition.to( ball, {x=_W+200, time=3000, onComplete=move_ballback} )
+    end
+  move_ball()
+
+  local function bird_position()
+    finalbird.x = -100
+    finalbird2.x = _W + 100
+    move_bird()
+  end
+
+  function move_bird()
+    local move_birdback = function()
+      transition.to( finalbird2, {x=-200, time=3000, onComplete=bird_position} )
+    end
+      transition.to( finalbird, {x=_W+300, time=3000, onComplete=move_birdback} )
+    end
+  move_bird()
 
   retry = display.newImage("images/retry.png")
     retry.x = _W/2
@@ -130,7 +183,9 @@ function scene:exitScene( event )
   display.remove(deadbird1)
   display.remove(deadbird2)
   display.remove(finalbird)
+  display.remove(finalbird2)
   display.remove(ball)
+  display.remove(ball2)
   display.remove(pontuacaoFinal)
 	menu:removeEventListener("tap", game_menu)
 	audio.stop()
